8bb80cef15a0de0e4e411ce90137c15be86e9c2e
NiteshTiwari89 <tiwarinitesh667@gmail.com>
2024-06-15 17:40:14 +0530

    cc

--- client/src/connection.jsx
+++ client/src/connection.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://school-inventory-management-system-oujy.vercel.app/', // Replace with your backend URL
+  withCredentials: true,
 });
 
 export default api;
