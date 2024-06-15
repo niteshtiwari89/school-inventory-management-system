0cda327ee63a3b9c441e54e42ffabc76419572ef
NiteshTiwari89 <tiwarinitesh667@gmail.com>
2024-06-15 16:47:35 +0530

    change

--- server/index.js
+++ server/index.js
@@ -295,7 +295,20 @@ const User =require('./models/userModel');
 // const env = require('')
 
 //MIddleware
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = ['https://school-inventory-management-system.vercel.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+}));
+
 app.use(express.json());
 app.use(bodyParser.json());
 //2) Route
