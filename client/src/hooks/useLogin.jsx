bed77876b5d47ec6f63f9f7538a81adaa3713703
NiteshTiwari89 <tiwarinitesh667@gmail.com>
2024-06-15 16:19:56 +0530

    change

--- client/src/hooks/useLogin.jsx
+++ client/src/hooks/useLogin.jsx
@@ -12,7 +12,7 @@ const loginUser =async(values) =>{
     try{
         setError(null);
         setLoading(true);
-        const res = await fetch('http://localhost:5000/api/auth/login',{
+        const res = await fetch('/api/auth/login',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
