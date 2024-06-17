14b6d06c92410e29ba2d9ff951077a15c0ee4191
NiteshTiwari89 <tiwarinitesh667@gmail.com>
2024-06-17 13:58:29 +0530

    scc

--- client/src/hooks/userSignup.jsx
+++ client/src/hooks/userSignup.jsx
@@ -21,7 +21,7 @@ const userSignup = () => {
       // Log the values being sent to the API
       // console.log("Sending values to API:", values);
 
-      const res = await axios.post('https://school-inventory-management-system-rep1/api/auth/signup', values, {
+      const res = await axios.post('https://server/api/auth/signup', values, {
         headers: {
           'Content-Type': 'application/json',
         },
