2b7ebf1085c6d13c4eac5f756b77f2d2e6cf566d
NiteshTiwari89 <tiwarinitesh667@gmail.com>
2024-06-27 19:31:18 +0530

    js

--- client/src/hooks/useLogin.jsx
+++ client/src/hooks/useLogin.jsx
@@ -12,7 +12,7 @@ const useLogin = () => {
     try {
       setError(null);
       setLoading(true);
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://school-inventory-management-system-server.vercel.app/api/auth/login", {
           method:'POST',
         headers: {
           "Content-Type": "application/json",
